f3c13b338f18d140af532e7ed78700987a5ea7e6
parkjuram <parkjuram@gmail.com>
2014-06-02 19:23:21 +0900

    add controller and user related view

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^registration/$', 'users.views.registration', name='registration'),
     url(r'^login/$', 'users.views.login', name='login'),
-    url(r'^login_page/$', 'users.views.login_view', name='login'),
+    url(r'^login_page/$', 'users.views.login_view', name='login_page'),
     url(r'^logout/$', 'users.views.logout', name='logout'),
     url(r'^update/$', 'users.views.update', name='update'),
 
