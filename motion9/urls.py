b4286e4da7ddd3fcc15e19d2a6102a2296e04423
parkjuram <parkjuram@gmail.com>
2014-06-09 20:11:29 +0900

    change url for media url

--- motion9/urls.py
+++ motion9/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,4 +14,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('users.urls')),
     url(r'', include('web.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
