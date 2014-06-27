418f0ba9e358deea35777bb09f0a22e7ee2961d8
kunbae_park <pgenius2@gmail.com>
2014-06-27 18:31:50 +0900

    from KB

--- web/static/js/common_js_mobile.js
+++ web/static/js/common_js_mobile.js
@@ -5,60 +5,108 @@
 $(function(){
     var renderCart = function(target, data){
         var keys = ['sets', 'products', 'custom_sets'];
+
+        target = $(target);
         var imgUrl;
         var productName;
         var productPrice;
         var howMany;
 
         var html = '';
-        var htmlPrepend = "<li>"
-                    + "<div class='cart-item-img'>"
-                    +   "<img src='"+imgUrl+"'/>"
-                    + "</div>"
-                    + "<div class='cart-item-info'>"
-                    +   productName
-                    + "</div>"
-                    + "<div class='cart-select-box-wrapper'>"
-                    + "     <lable>수량 : </lable>"
-                    + "     <select data-shadow='false' data-corner='false'>";
 
-        var htmlPost = '    </select>'
-                    + '     <div class="clearfix"></div>'
-                    + '</div>
-                    + '가격 : '
-                    + productPrice
-                    + '원 <div class="clearfix"></div>'
-                    + '</li>';
 
         for(var j = 0; j < keys.length ; j++){
-            var list =  data[keys[i]];
+            var list =  data[keys[j]];
             for(var i = 0 ; i < list.length ; i++){
                 html = '';
                 productPrice = list[i].discount_price;
                 productName = list[i].name;
                 imgUrl = list[i].big_img_url;
+                howMany = list[i].item_count;
+
+
+                var htmlPrepend = "<li>"
+                    + "<div class='cart-item-img'>"
+                    +   "<img src='"+imgUrl+"'/>"
+                    + "</div>"
+                    + "<div class='cart-item-info'>"
+                    +       productName
+                    + "     <div class='cart-select-box-wrapper'>"
+                    + "         <lable>수량 : </lable>"
+                    + "         <select data-shadow='false' data-corner='false'>";
+
+                var htmlPost = '</select>'
+                    + '     </div>'
+                    + '     <div class="clearfix"></div>'
+                    + '가격 : '
+                    + productPrice + '원'
+                    + '</div>'
+                    + '<div class="clearfix"></div>'
+                    + '</li>';
 
                 html += htmlPrepend;
-
+                for(var k = 0 ; k < howMany + 3 ; k++){
+                    if(k == howMany)
+                        html += '<option selected="selected">'+k+'</option>';
+                    else
+                        html += '<option>'+k+'</option>';
+                }
                 html += htmlPost;
                 target.append($(html));
             }
         }
     };
 
-    $('#cartPanel').on('panelopen', function(){
-        $.ajax({
-              url: '/user/mypage/cart/json/',
-              dataType: 'json',
-              async : true,
-              type:'POST',
 
-              success: function(data){
-                  renderCart('#cartPanel .custom-menu-list', data);
-              },
-              error:function(jqXHR, textStatus, errorThrown){
-                  console.log(textStatus);
-              }
-		});
+
+    $('#cartPanel').on('panelopen', function(){
+        var target = $('#cartPanel .custom-menu-list');
+
+        if(false && target.length != 0){
+            target.html('');
+            $.ajax({
+                  url: '/user/mypage/cart/json/',
+                  dataType: 'json',
+                  async : true,
+                  type:'get',
+
+                  success: function(data){
+
+                      renderCart('#cartPanel .custom-menu-list', data);
+                      $.mobile.loading('hide');
+                      $(window).scroll();
+
+                  },
+                  error:function(jqXHR, textStatus, errorThrown){
+                      console.log(textStatus);
+                  }
+            });
+        }
     });
+
+
+    $('#testBtn').click(function(e){
+        e.preventDefault();
+        $.mobile.loading('show');
+        var target = $('#cartPanel .custom-menu-list');
+
+        if(target.length != 0){
+            target.html('');
+            $.ajax({
+                  url: '/user/mypage/cart/json/',
+                  dataType: 'json',
+                  async : true,
+                  type:'get',
+                  success: function(data){
+
+                      renderCart('#cartPanel .custom-menu-list', data);
+                      $.mobile.loading('hide');
+                      $('#cartPanel').panel('open');
+                  },
+                  error:function(jqXHR, textStatus, errorThrown){
+                      console.log(textStatus);
+                  }
+            });
+        }
+        });
 });
--- web/static/js/set_detail.js
+++ web/static/js/set_detail.js
@@ -10,12 +10,42 @@ $(function(){
    });
 
    var mySwiper = $('.swiper-container').swiper({
-    //Your options here:
-    mode:'horizontal',
-    loop: true,
-    calculateHeight : true,
-    pagination: '.pagination',
-    paginationClickable: true
-    //etc..
-  });
+        //Your options here:
+        mode:'horizontal',
+        loop: true,
+        calculateHeight : true,
+        pagination: '.pagination',
+        paginationClickable: true
+        //etc..
+   });
+
+   $('.view-product-btn').click(function(e){
+        e.preventDefault();
+        var id = $(this).attr('data-product');
+        var dialog = $('#productDetailDialog');
+        var img = $('#popupDetailImg').find('img');
+        var name = $('#popupDetailName');
+        var desc = $('#popupDetailDesc');
+        var btn = $('#popupDetailBtn');
+
+        $.ajax({
+              url: '/mobile/product/'+id+'/json/',
+              dataType: 'json',
+              async : true,
+              type:'POST',
+              success: function(data){
+                  console.log(data);
+                  img.attr('src', data.big_img_url);
+                  name.text(data.name);
+                  desc.text(data.description);
+                  btn.attr('href', '/mobile/product/'+id);
+
+                  dialog.popup('open');
+              },
+              error:function(jqXHR, textStatus, errorThrown){
+
+              }
+		});
+
+    });
 });
