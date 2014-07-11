8a70630cd72f1760cb8a3469aa2890ba66dbf3a6
kunbae_park <pgenius2@gmail.com>
2014-07-12 00:42:05 +0900

    from KB

--- web/static/js/shopping_set_web.js
+++ web/static/js/shopping_set_web.js
@@ -9,13 +9,29 @@ $(function(){
       max: 500000,
       values: [ 70000, 140000 ],
       slide: function( event, ui ) {
-          $( "#sPrice" ).text( ui.values[ 0 ]);
-          $( "#fPrice" ).text( ui.values[ 1 ]);
+          $( "#sPrice" ).text( numberFormatter(ui.values[ 0 ]));
+          $( "#fPrice" ).text( numberFormatter(ui.values[ 1 ]));
       }
     });
 
-    $( "#sPrice" ).text( $( "#priceRange" ).slider( "values", 0 ));
-    $( "#fPrice" ).text( $( "#priceRange" ).slider( "values", 1 ));
+    $( "#sPrice" ).text( numberFormatter($( "#priceRange" ).slider( "values", 0 )));
+    $( "#fPrice" ).text( numberFormatter($( "#priceRange" ).slider( "values", 1 )));
+
+    $('.filter-btn').click(function(e){
+        e.preventDefault();
+        var min = $( "#priceRange" ).slider( "values", 0 );
+        var max = $( "#priceRange" ).slider( "values", 1 );
+
+        var url = location.href;
+        var qIdx;
+        if((qIdx = url.indexOf('?')) != -1){
+           url = url.substring(0, qIdx-1);
+           url += '?price_max='+max+'&price_min='+min;
+        }else{
+           url += '?price_max='+max+'&price_min='+min;
+        }
+        location.href = url;
+    });
 
 
     $('.set-item-info').css({opacity:0.9});
