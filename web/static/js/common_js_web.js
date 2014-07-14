2741b006d583ec5b43269f1185d54477d24a4bf0
kunbae_park <pgenius2@gmail.com>
2014-07-14 21:50:23 +0900

    from KB

--- web/static/js/common_js_web.js
+++ web/static/js/common_js_web.js
@@ -23,6 +23,7 @@ $(function(){
         }
     });
 
+
     $('.login-submit-btn').click(function(e){
         e.preventDefault();
         var formType = $(this).attr('data-form');
@@ -37,7 +38,70 @@ $(function(){
     $('.login-btn').click(function(e){
         e.preventDefault();
         $('#loginBox').jqmShow();
+
+
     });
+
+    function loginCheck(email){
+        $.ajax({
+              url: "/user/logincheck/",
+              dataType: 'json',
+              async : true,
+              type:'POST',
+              data:{email : email},
+              success: function(data){
+                  if(data == 'valid')
+                    location.href= 'login success url';
+                  else
+                    location.href= '/user/registration_page';
+              },
+              error:function(jqXHR, textStatus, errorThrown){
+                  console.log(textStatus);
+              }
+		});
+    }
+
+
+    function fbLoginCallback(response) {
+        console.log(response);
+        if (response.status === 'connected') {
+          // Logged into your app and Facebook.
+            //testAPI();
+            loginCheck();
+            console.log('already logged in');
+        } else if (response.status === 'not_authorized') {
+
+            window.location = encodeURI("https://www.facebook.com/dialog/oauth?client_id=1450591788523941&redirect_uri=http://"+location.host+'/user/registration_page'+"&response_type=token&scope=public_profile,email,user_friends");
+
+            /*
+            FB.login(function(response) {
+              if (response.status === 'connected') {
+                // Logged into your app and Facebook.
+                 console.log('logged in');
+              } else if (response.status === 'not_authorized') {
+                // The person is logged into Facebook, but not your app.
+                console.log('facebook logged in but not the app');
+              } else {
+                // The person is not logged into Facebook, so we're not sure if
+                // they are logged into this app or not.
+                  console.log('wtff');
+              }
+            }, {scope: 'public_profile,email,user_friends'});
+            */
+        } else {
+          // The person is not logged into Facebook, so we're not sure if
+          // they are logged into this app or not.
+          console.log('both required.');
+        }
+    }
+
+
+    $('.fb-login-btn').click(function(e){
+        e.preventDefault();
+
+        FB.getLoginStatus(fbLoginCallback);
+    });
+
 
     var subMenuInitialize = function(){
 
---
+++ web/static/js/register_web.js
@@ -0,0 +1,55 @@
+$(function(){
+
+    var registerPageInit = function(response) {
+        console.log(response);
+        if (response.status === 'connected') {
+            wireDataWithForm();
+        } else if (response.status === 'not_authorized') {
+
+        } else {
+
+        }
+    }
+
+
+    function wireDataWithForm(){
+        var name  = $('#name');
+        var email  = $('#email');
+
+        FB.api('/me', function(response) {
+
+          email.val(response.email);
+          name.val(response.last_name + response.first_name);
+
+          if(response.gender == 'male'){
+              $('#sex_m').attr('checked', 'checked');
+          }else{
+              $('#sex_f').attr('checked', 'checked');
+          }
+
+          console.log(response);
+        });
+    }
+
+    $.ajaxSetup({ cache: true });
+      $.getScript('//connect.facebook.net/ko_kr/all.js', function(){
+        FB.init({
+          appId: '1450591788523941',
+          cookie: true,  // enable cookies to allow the server to access
+          // the session
+          xfbml: true,  // parse social plugins on this page
+          version: 'v2.0' // use version 2.0
+        });
+
+         FB.getLoginStatus(registerPageInit);
+      });
+
+
+
+    $('.fb-join-btn').click(function(e){
+        e.preventDefault();
+        window.location = encodeURI("https://www.facebook.com/dialog/oauth?client_id=1450591788523941&redirect_uri=http://"+location.host+'/user/registration_page'+"&response_type=token&scope=public_profile,email,user_friends");
+    });
+
+
+});
