1de2791374334621374ec5ee6cc377525115731b
parkjuram <parkjuram@gmail.com>
2014-07-22 18:04:49 +0900

    blog review admin page setting

--- web/admin.py
+++ web/admin.py
@@ -9,7 +9,14 @@ class BrandAdmin(admin.ModelAdmin):
     # list_display_links = ('name_kor', 'is_repr_to_eng')
     list_filter = ('name_eng', 'name_kor', 'is_repr_to_eng', 'is_domestic')
 
+class BlogReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'writer', 'url', 'big_img_url', 'small_img_url', 'review_created_time', 'summary')
+    list_editable = ('writer', 'url', 'big_img_url', 'small_img_url', 'review_created_time', 'summary')
+    list_display_links = ('product',)
+    # list_filter = ('product', 'writer', 'url', 'big_img_url', 'small_img_url', 'review_created_time', 'summary')
+
 admin.site.register(Brand, BrandAdmin)
+admin.site.register(BlogReview, BlogReviewAdmin)
 
 admin.site.register(Category)
 admin.site.register(Product)
@@ -19,4 +26,3 @@ admin.site.register(Set)
 admin.site.register(SetProduct)
 admin.site.register(ChangeableProduct)
 admin.site.register(ChangeableProductInfo)
-admin.site.register(BlogReview)
