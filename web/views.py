dd043235d8bd9926e9410def309922191bb7c371
parkjuram <parkjuram@gmail.com>
2014-08-11 19:26:54 +0900

    mileage apply

--- web/views.py
+++ web/views.py
@@ -649,7 +649,7 @@ def payment_return_explore_view(request):
             detail_response_message=detail_response_message[0]
         )
 
-        user_profile.mileage = int(user_profile.mileage) - ( int(OrderTempInfo.objects.get(order_id=order_id).original_amount) - int(auth_amount[0]))
+        user_profile.mileage = int(user_profile.mileage) - ( int(OrderTempInfo.objects.get(order_id=order_id).original_amount) - int(auth_amount[0])) + int(auth_amount[0])/100
         user_profile.save()
 
         carts = Cart.objects.filter(order_id=order_id).all()
