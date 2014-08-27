c19f7b7000724520c2b4cdc45b925f04a287ef57
parkjuram <parkjuram@gmail.com>
2014-08-27 19:08:00 +0900

    add advertisement table to admin

--- foradmin/admin.py
+++ foradmin/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import MainImage
+from .models import MainImage, Advertisement
+
 
 class MainImageAdmin(admin.ModelAdmin):
     list_display = ('name', 'image')
-
 admin.site.register(MainImage, MainImageAdmin)
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ( 'type', 'image', 'link')
+    list_editable = ('image', 'link')
+
+admin.site.register(Advertisement, AdvertisementAdmin)
