0c4524cfbf57d5d5dde59357f1db04dfc4196952
savekhg@gmail.com <savekhg@gmail.com>
2014-10-22 17:45:58 +0900

    javascript-minimize-hk

---
+++ web/static/js/report.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    var container = document.querySelector('#productGrid');
+    var msnry;
+
+
+    imagesLoaded(container, function(){	//when all images are loaded, it is triggered
+
+        msnry = new Masonry( container, {	// api for displaying images as a grid
+          // options
+          columnWidth: '.product-item',
+          isResizeBound : false,
+          gutter : 0,
+          itemSelector: '.product-item'
+        });
+    });
+
+    imagesLoaded(container, function(){	//when all images are loaded, it is triggered
+
+        msnry = new Masonry( container, {	// api for displaying images as a grid
+          // options
+          columnWidth: '.product-detail',
+          isResizeBound : false,
+          gutter : 0,
+          itemSelector: '.product-detail'
+        });
+    });
+
+});
