9f29d1ae2177fd28043e7db36284ec3512504881
parkjuram <parkjuram@gmail.com>
2014-11-05 21:17:40 +0900

    mobile-login-bug-fix

--- web/static/js/login.js
+++ web/static/js/login.js
@@ -1,39 +1,13 @@
-$(function(){
-
-    function loginCheck(email){
-        $.ajax({
-              url: "/user/logincheck/",
-              dataType: 'json',
-              async : true,
-              type:'POST',
-              data:{email : email},
-              success: function(data){
-                  if(data == 'valid')
-                    location.href= 'login success url';
-                  else
-                    location.href= '/user/mobile_registration_page';
-              },
-              error:function(jqXHR, textStatus, errorThrown){
-                  console.log(textStatus);
-              }
-		});
-    }
-
+$( document ).on( "pageinit", function( event ) {
     function fbLoginCallback(response) {
         if (response.status === 'connected') {
-          // Logged into your app and Facebook.
-            //testAPI();
-
             FB.api('/me', function(user_info) {
                 $('#fbEmail').val(user_info.email);
                 $('#fbToken').val(response.authResponse.accessToken);
                 $('#facebookLoginForm').submit();
             });
-
         } else if (response.status === 'not_authorized') {
-
             window.location = encodeURI("https://www.facebook.com/dialog/oauth?client_id=1450591788523941&redirect_uri=http://"+location.host+'/user/registration_page'+"&response_type=token&scope=public_profile,email,user_friends");
-
             /*
             FB.login(function(response) {
               if (response.status === 'connected') {
@@ -61,6 +35,28 @@ $(function(){
 
         FB.getLoginStatus(fbLoginCallback);
     });
+});
+
+$(function(){
+
+    function loginCheck(email){
+        $.ajax({
+              url: "/user/logincheck/",
+              dataType: 'json',
+              async : true,
+              type:'POST',
+              data:{email : email},
+              success: function(data){
+                  if(data == 'valid')
+                    location.href= 'login success url';
+                  else
+                    location.href= '/user/mobile_registration_page';
+              },
+              error:function(jqXHR, textStatus, errorThrown){
+                  console.log(textStatus);
+              }
+		});
+    }
 
     $('.login-box, .login-form').keypress(function(e){
         if(e.keyCode == 13){
