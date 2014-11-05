2f655ce77c15f3e2825e4b37984805a102e39b07
savekhg@gmail.com <savekhg@gmail.com>
2014-11-05 20:35:49 +0900

    mobile-report-hk

--- motion9/urls.py
+++ motion9/urls.py
@@ -10,11 +10,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'motion9.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('users.urls')),
     url(r'', include('web.urls')),
     url(r'mobile/', include('mobile.urls')),
     url(r'foradmin/', include('foradmin.urls', namespace='foradmin')),
     url(r'^accounts/', include('registration.backends.default.urls')),
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
