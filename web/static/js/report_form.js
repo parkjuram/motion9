ec48caa9e0664852aa89d2783ab71160a3625275
savekhg@gmail.com <savekhg@gmail.com>
2014-11-09 21:32:40 +0900

    mobile-report-form-ui

--- web/static/js/report_form.js
+++ web/static/js/report_form.js
@@ -1,41 +1,35 @@
-$(function(){
+$(function () {
 
-    $('.report-btn').click(function (e) {
+    $('#report-btn').click(function (e) {
+        e.preventDefault();
 
         var errorMessage = null;
 
-//        if($('#postalcode').val()=='' || $('#basicAddress').val()==''){
-//            errorMessage = '주소를 입력해 주십시오.';
-//        }else if($('#sendTo').val()==''){
-//            errorMessage = '수취인 이름을 입력해 주십시오.';
-//        }else if($('#phone1').val()=='' || $('#phone2').val()=='' || $('#phone3').val()==''){
-//            errorMessage = '수취인 전화번호를 입력해 주십시오.';
-//        }else if($('#cart-sum-price').text()==''){
-//            errorMessage = '결제금액 1000원 미만은 결제되지 않습니다.'
-//        }
-//
-//        if ( errorMessage != null ) {
-//            alert( errorMessage );
-//        }else{
-            $.ajax({
-                url: urlReportForm,
-                dataType: 'json',
-                async : true,
-                type:'POST',
-                data: {
-                    'survey_id': $('').val(),
-                    'comments': $('#phone1').val()+"-"+$('#phone2').val()+"-"+$('#phone3').val(),
-                    'options': $('#postalcode').val()
-                },
+        var options = [];
 
-                success: function (data) {
-                    if (data.success) {
-                        startPayment();
-                    }
+        $.each( $('fieldset[data-role="controlgroup"] :checked'), function( index ) {
+           options.push( $(this).val() );
+        });
+
+
+        $.ajax({
+            url: urlRequestSurvey,
+            dataType: 'json',
+            async: true,
+            type: 'POST',
+            data: {
+                'survey_id': $(this).attr('survey_id'),
+                'comments': $('#reportText').val().trim(),
+                'options': options
+            },
+
+            success: function (data) {
+                if (data.success) {
+                    alert('request success!');
                 }
+            }
 
-            });
-//        }
+        });
 
     });
 });
