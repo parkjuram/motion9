b6515bd941b8e379152e50a14d7c292421831821
savekhg@gmail.com <savekhg@gmail.com>
2014-11-11 21:50:17 +0900

    web-report-form-ui

---
+++ web/static/js/report_form_web.js
@@ -0,0 +1,45 @@
+$(function () {
+
+    $('#report-btn').click(function (e) {
+        e.preventDefault();
+
+        var errorMessage = null;
+
+        var options = [];
+
+        $.each( $('fieldset[data-role="controlgroup"] :checked'), function( index ) {
+           options.push( $(this).val() );
+        });
+
+
+        $.ajax({
+            url: urlRequestSurvey,
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                'survey_id': $(this).attr('survey_id'),
+                'comments': $('#reportText').val().trim(),
+                'options': options
+            },
+
+            success: function (data) {
+                if (data.success) {
+                    alert('request success!');
+                }
+            }
+
+        });
+
+    });
+
+
+});
+
+
+    $(document).ready(function() {
+        $("#survey-section-container").steps({
+            bodyTag: "section",
+            transitionEffect: "slideLeft",
+            autoFocus: true
+        });
+    });
