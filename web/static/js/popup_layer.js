452ce51473d609a8b7dc1a58bba36eb844a5cb04
savekhg@gmail.com <savekhg@gmail.com>
2014-11-23 16:47:19 +0900

    report-form-hk

--- web/static/js/popup_layer.js
+++ web/static/js/popup_layer.js
@@ -2,8 +2,9 @@ $(function () {
 
     function layer_open(el) {
 
-        var temp = $('#' + el);
-        var bg = temp.prev().hasClass('bg'); //dimmed 레이어를 감지하기 위한 boolean 변수
+        var bg, temp;
+        temp = $('#' + el);
+        bg = temp.prev().hasClass('bg'); //dimmed 레이어를 감지하기 위한 boolean 변수
 
         if (bg) {
             $('.layer').fadeIn();   //'bg' 클래스가 존재하면 레이어가 나타나고 배경은 dimmed 된다.
@@ -32,8 +33,14 @@ $(function () {
 
         });
 
+
         $('.layer .bg').click(function (e) {  //배경을 클릭하면 레이어를 사라지게 하는 이벤트 핸들러
 
+            $('.layer').fadeOut();
+            e.preventDefault();
+        });
+
+        $('#layer-btn').click(function(e) {
             $('.layer').fadeOut();
             e.preventDefault();
         });
@@ -41,8 +48,8 @@ $(function () {
 
     window.onload = function () {
 
-//        layer_open('layer2');
-//        return false;
+        layer_open('layer2');
+        return false;
 
     }
 
