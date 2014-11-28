422aa8cd3f40f68919ce04ce64020157f978efc3
parkjuram <parkjuram@gmail.com>
2014-11-28 21:42:58 +0900

    no message

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,5 +1,5 @@
 $(document).on('pageinit', '.ui-page',function(event){
     setTimeout(function () {
         $('.selector').popup('open');
-    }, 0);//Note the comment below from @Taifun.
-})​
+    }, 0);
+})
