7daf6e2d43ee7d7fce3678fc8f0c92620656d0d6
parkjuram <parkjuram@gmail.com>
2014-11-28 17:29:31 +0900

    no message

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,7 +1,4 @@
 $(document).on("pageshow", function() {
-    var popup = setInterval(function(){
-        $(".selector").popup("open");
-        clearInterval(popup);
-    },1);
+    $(".selector").popup("open");
 });
 
