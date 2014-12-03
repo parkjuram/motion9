dcec981d456b292b7e96c90fbdc1cc18d1fd9423
savekhg@gmail.com <savekhg@gmail.com>
2014-12-03 19:26:29 +0900

    popup-fix-hk

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,5 +1,9 @@
 $(document).on('pageshow', '.ui-page',function(event){
     setTimeout(function () {
-        $('.selector').popup('open');
+        $('.selector').popup('open', {
+          x: 0,
+          y: 0,
+//          transition: "reverse slide",
+        });
     }, 2000);
 });
