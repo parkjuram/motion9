e56b5ef7be53585369a7a21ff7fe916fd23d5059
parkjuram <parkjuram@gmail.com>
2015-02-09 15:57:17 +0900

    supervisor page edit

--- supervisor/tasks.py
+++ supervisor/tasks.py
@@ -12,23 +12,6 @@ logger = get_task_logger(__name__)
 # def analysis_product(self, logger, querys):
 @task(bind=True)
 def analysis_product(self, querys):
-    # """Background task that runs a long function with progress reports."""
-    # verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
-    # adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
-    # noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
-    # message = ''
-    # total = random.randint(10, 50)
-    # for i in range(total):
-    #     if not message or random.random() < 0.25:
-    #         message = '{0} {1} {2}...'.format(random.choice(verb),
-    #                                           random.choice(adjective),
-    #                                           random.choice(noun))
-    #     self.update_state(state='PROGRESS',
-    #                       meta={'current': i, 'total': total,
-    #                             'status': message})
-    #     time.sleep(1)
-    # return {'current': 100, 'total': 100, 'status': 'Task completed!',
-    #         'result': 42}
     blog_review_link_scrapper = BlogReviewLinkScrapper()
     blog_url_list = blog_review_link_scrapper.startScrapping(query_item_list = querys)
 
--- supervisor/views.py
+++ supervisor/views.py
@@ -119,7 +119,7 @@ class ProductAnalysisView(SuperuserRequiredMixin, View):
 
 class UserSurveyListView(SuperuserRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        user_surveys = UserSurvey.objects.select_related('user').order_by('-created').all()[:5]
+        user_surveys = UserSurvey.objects.select_related('user').order_by('-created').all()
         user_surveys_ = []
         for user_survey in user_surveys:
             user_survey_ = {
