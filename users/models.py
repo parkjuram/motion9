dcd4a33e8ad1688696d250136be9614beab2cd11
parkjuram <parkjuram@gmail.com>
2015-03-27 10:34:20 +0900

    user migration ready

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db.models import signals
 
 from datetime import datetime
